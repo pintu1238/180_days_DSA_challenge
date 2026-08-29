6d568c6e6eaf88bc53407084eccd21e8547d50e9
Pintu Kumar <112333989+pintu1238@users.noreply.github.com>
2026-08-29 10:31:28 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+ 
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        for(; j<nums.size(); j++)
+        {
+            nums[j]=0;
+        }
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
